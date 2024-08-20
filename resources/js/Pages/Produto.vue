e0a7db026a01825d474922bac977c06b69af0d93
Marco Antonio <marco.nunes@hfa.mil.br>
2024-08-20 16:30:10 -0300

    ajustes

--- resources/js/Pages/Produto.vue
+++ resources/js/Pages/Produto.vue
@@ -1,24 +1,48 @@
 <template>
     <HeaderView :user="user" />
-    <section>
-        <div>
-            
+    <main class="">
+        <div class="bg-[red] TelaD">
+            <p>Distrito Federal / <span>Laptops</span></p>
+            <div>
+               <div class="max-w-[700px] max-h-[650px] w-full h-full">
+                    <GalleryView :items="items" />
+                </div>
+            </div>
         </div>
-    </section>
+    </main>
 </template>
 
 <script>
-import HeaderView from '@/Components/HeaderView.vue'
+import HeaderView from '@/Components/HeaderView.vue';
+import GalleryView from '@/Components/GalleryView.vue';
 
 export default {
     name:"ProdutoView",
     props:['user'],
     components:{
-        HeaderView
+        HeaderView,
+        GalleryView
+    },
+    data(){
+        return{
+            items: [
+                {
+                    imageUrl: "/img/caneca.jpg",
+                },
+                {
+                    imageUrl: "/img/caneca-branca.jpg",
+                },
+                {
+                    imageUrl: "/img/caneca.jpg",
+                },
+            ],
+        }
     }
 }
 </script>
 
 <style>
-
+.TelaD{
+    height: calc(100vh - 152px);
+}
 </style>
